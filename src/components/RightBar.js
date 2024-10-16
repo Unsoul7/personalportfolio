52d7d4dff11679e10eed8e58a5010ad9997ef172
unsoul7 <disketteit@gmail.com>
2024-10-16 16:15:48 +0530

    asdf

--- src/components/RightBar.js
+++ src/components/RightBar.js
@@ -1,9 +1,9 @@
-import React,{useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import '../styles/rightbar.css'
 
 export const RightBar = () => {
 
-    const talents = ['Web Dev','Video Editor','Social Media Manager','Fullstack Dev']
+    const talents = ['Web Dev', 'Video Editor', 'Social Media Manager', 'Fullstack Dev']
     const [Prof, setProf] = useState(talents[0])
 
     useEffect(() => {
@@ -22,14 +22,27 @@ export const RightBar = () => {
 
     return (
         <div className="right-panel">
+
             <div id="profile-full-name">
                 <p><span>Aman</span> Prajapati</p>
             </div>
+
             <div id="profile-professional-role"><p>{Prof}</p></div>
+
             <div id="profile-user-tagline"><p>We appreciate your trust greatly our clients choose us & our products because they know we are the best.</p></div>
+
             <div id="action-btns">
                 <a href="">View Work</a>
                 <a href="">Contact Me</a>
+            </div>
+
+            <div id="profile-about-me">
+                <div id="aboutme-line">
+                    <p>About Me.</p>
+                    <div></div>
+                </div>
+
+                <p>I’m a multi-skilled professional with expertise in web development and content creation. Proficient in HTML, CSS, JavaScript, Python, Node.js, Express, React, and MongoDB, I build scalable web applications. With Next.js and strong data structure knowledge, I handle both front-end and back-end development efficiently.</p>
             </div>
 
         </div>
