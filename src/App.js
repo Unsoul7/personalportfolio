8ee0f1165c5b7d145f957ea7dfa68b822798960d
unsoul7 <disketteit@gmail.com>
2024-10-19 18:31:01 +0530

    responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import './App.css';
 import { LeftBar } from './components/LeftBar';
 import { RightBar } from './components/RightBar';
+import './styles/responsive.css'
+import menu from './assets/menu.png'
 
 function App() {
   return (
     <main>
+        <img id='menu-btn' src={menu} alt="" />
         <LeftBar />
         <RightBar />
     </main>
